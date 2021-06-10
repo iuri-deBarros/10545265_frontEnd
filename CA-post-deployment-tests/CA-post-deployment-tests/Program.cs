875e91b4464f497a1fd64bbaf0dc59d097f763b0
EhtishamYasin <ehtihsam@xlr8solutions.com>
2021-06-10 20:03:46 +0100

    Revert "update v2 13.04"
    This reverts commit 027b10b2f5152fa844dc08c4f9ed6983c6874e54.

---
+++ CA-post-deployment-tests/CA-post-deployment-tests/Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA_post_deployment_tests
+{
+
+ // public IWebDriver driver = new ChromeDriver("C:\Users\kezze\OneDrive\Desktop");
+
+  class Program
+    {
+        static void Main(string[] args)
+        {
+
+  
+
+
+
+
+    }
+    }
+}
